62de56860fff7ed1f3d05b396e3b3cd5ccd2b3f2
lifei <lifei.vip@outlook.com>
2017-02-23 01:37:08 +0800

    fix: 修复无法执行 bash 脚本的问题

--- init.sh
+++ init.sh
@@ -3,13 +3,13 @@
 umask 022
 set -e
 
-apk --update upgrade && apk add --update git curl && rm -rf /var/cache/apk/* /tmp/*
+apk --update upgrade && apk add --update git curl bash && rm -rf /var/cache/apk/* /tmp/*
 
 adduser -h ${GERRIT_HOME} -S -D ${GERRIT_USER}
 curl -fSL -o ${GERRIT_WAR} https://gerrit-releases.storage.googleapis.com/gerrit-${GERRIT_VERSION}.war
 
 cat > /usr/local/bin/init-gerrit.sh <<EOF
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 java -jar ${GERRIT_WAR} init --batch -d ${GERRIT_ROOT}
 EOF
 
